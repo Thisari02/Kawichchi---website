286358440ef5102681328051293b61babf4248a0
Kawichchi Developer <dev@kawichchi.com>
2026-02-04 11:14:06 +0530

    refactor: Update App.jsx - remove Contact and Footer components, focus on core sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
-export default function App() {
+function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <About />
       <Projects />
-      <Contact />
-      <Footer />
     </>
   );
 }
+
+export default App;
