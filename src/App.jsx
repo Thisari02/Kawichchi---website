ae92ca9b2756b62c4279530e3ffa577b5cf994c5
Kawichchi Developer <dev@kawichchi.com>
2026-02-04 11:23:48 +0530

    refactor: Update App.jsx - remove Factory component, streamline to 6 core sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Factory from "./components/Factory";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <About />
       <Projects />
       <Contact />
-      <Factory />
       <Footer />
     </>
   );
